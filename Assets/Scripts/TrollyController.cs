c3c52338b99ed9a2d75a3ee5ccaf00348488366d
Robert Geil <robert.geil999@gmail.com>
2018-11-30 22:58:52 -0800

    Initial commit

---
+++ Assets/Scripts/TrollyController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrollyController : MonoBehaviour {
+
+
+    Vector3 initPos;
+
+	// Use this for initialization
+	void Start () {
+        initPos = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        x = Mathf.Round(x);
+        this.transform.position = initPos + new Vector3(x, 0, 0);
+	}
+}
